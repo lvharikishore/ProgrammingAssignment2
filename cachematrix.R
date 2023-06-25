c280d93147fded142f7c9f0d5657382ee74e5b35
hkanalyticbytes <64832094+hkanalyticbytes@users.noreply.github.com>
2023-06-26 00:22:21 +0530

    added comments

--- cachematrix.R
+++ cachematrix.R
@@ -1,17 +1,27 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Couple of methods that cache the inverse of a matrix
 
-## Write a short comment describing this function
 
+## This function creates a special "matrix" object that can cache its inverse
 makeCacheMatrix <- function(x = matrix()) {
+  ## Initialise the inverse variable
   im <- NULL
+  
+  ##setter for matrix
   set <- function(y) {
     x <<- y
     im <<- NULL
   }
+  
+  ## getter for matrix , returns matrix
   get <- function() x
+  
+  ## setting inverse of the matrix
   setim <- function(inv) im <<- inv
+  
+  ## getting the inverse of the matrix
   getim <- function() im
+  
+  ## list of functions
   list(set = set, get = get,
        setim = setim,
        getim = getim)
@@ -19,17 +29,29 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 
 
-## Write a short comment describing this function
+## This function computes the inverse of the special "matrix" returned by makeCacheMatrix above. 
+## If the inverse has already been calculated (and the matrix has not changed), 
+## then the cachesolve should retrieve the inverse from the cache
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  ## Return a matrix that is the inverse of 'x'
   im <- x$getim()
+  
+  ## return the inverse if its already set in cache
   if(!is.null(im)) {
     message("getting cached data")
     return(im)
   }
+  
+  ## get the matrix
   data <- x$get()
+  
+  ## computer inverse of matrix
   im <- solve(data, ...)
+  
+  ## Set the inverse to cache
   x$setim(im)
+  
+  ## return inverse matrix
   im
 }
