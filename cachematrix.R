ee83ed36be7b584e59c6441c016931beb53d9ec4
hkanalyticbytes <64832094+hkanalyticbytes@users.noreply.github.com>
2023-06-25 23:55:43 +0530

    assignment

--- cachematrix.R
+++ cachematrix.R
@@ -4,6 +4,17 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
+  im <- NULL
+  set <- function(y) {
+    x <<- y
+    im <<- NULL
+  }
+  get <- function() x
+  setim <- function(inv) im <<- inv
+  getim <- function() im
+  list(set = set, get = get,
+       setim = setim,
+       getim = getim)
 
 }
 
@@ -12,4 +23,13 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  im <- x$getim()
+  if(!is.null(im)) {
+    message("getting cached data")
+    return(im)
+  }
+  data <- x$get()
+  im <- solve(data, ...)
+  x$setim(im)
+  im
 }
